d8cafbc5c43de80a481756160a343371a3db47e6
Atlee Circuitree <atleecircuitree@gmail.com>
2023-11-21 17:55:44 -0500

    Teleop skeleton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTestRobot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleOpTestRobot.java
@@ -1,34 +1,32 @@
 package org.firstinspires.ftc.teamcode;
 
-import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.IMU;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.IMU;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
 import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 
 @TeleOp
-public class TeleOp2023 extends LinearOpMode {
+public class TeleOpTestRobot extends LinearOpMode {
     @Override
     public void runOpMode() throws InterruptedException {
         // Declare our motors--
         // Make sure your ID's match your configuration
-        DcMotor lary = hardwareMap.dcMotor.get("lary");
+        DcMotor frontLeftMotor = hardwareMap.dcMotor.get("frontLeftMotor");
         DcMotor backLeftMotor = hardwareMap.dcMotor.get("backLeftMotor");
         DcMotor frontRightMotor = hardwareMap.dcMotor.get("frontRightMotor");
         DcMotor backRightMotor = hardwareMap.dcMotor.get("backRightMotor");
-        DcMotor rightSlideMotor = hardwareMap.dcMotor.get("rightSlideMotor");
-        DcMotor leftSlideMotor = hardwareMap.dcMotor.get("leftSlideMotor");
-        Servo stephanie = hardwareMap.servo.get("stephanie");
-        Servo goodServo = hardwareMap.servo.get("goodServo");
+
+        Servo testServo = hardwareMap.servo.get("testServo");
 
         //DcMotor leftSlideMotor = hardwareMap.dcMotor.get("leftSlideMotor");
         //DcMotor rightSlideMotor = hardwareMap.dcMotor.get("rightSlideMotor");
 
-        lary.getCurrentPosition();
+        frontLeftMotor.getCurrentPosition();
 
         // Reverse the right side motors. This may be wrong for your setup.
         // If your robot moves backwards when commanded to go forwards,
@@ -48,21 +46,14 @@ public class TeleOp2023 extends LinearOpMode {
 
         waitForStart();
 
-        // Function Speeds
-        double driveSpeed = .5;
-        double armSpeed = .5;
-        double intakeSpeed = .3;
-
-        // Function Positions
-        double clawOpen = .3;
-        double clawClose = .7;
+        double speed = .5;
 
         if (isStopRequested()) return;
 
         while (opModeIsActive()) {
-            double y = -gamepad1.left_stick_y * driveSpeed; // Remember, Y stick value is reversed
-            double x = gamepad1.left_stick_x * driveSpeed;
-            double rx = gamepad1.right_stick_x * driveSpeed;
+            double y = -gamepad1.left_stick_y * speed; // Remember, Y stick value is reversed
+            double x = gamepad1.left_stick_x * speed;
+            double rx = gamepad1.right_stick_x * speed;
 
             // This button choice was made so that it is hard to hit on accident,
             // it can be freely changed based on preference.
@@ -71,35 +62,13 @@ public class TeleOp2023 extends LinearOpMode {
                 imu.resetYaw();
             }
 
-            // Moves the arm up and down
-            if (gamepad1.left_trigger > armSpeed)  {
-                leftSlideMotor.setPower(armSpeed);
-                rightSlideMotor.setPower(armSpeed);
-            }  else if (gamepad1.right_trigger > armSpeed) {
-                leftSlideMotor.setPower(-armSpeed);
-                rightSlideMotor.setPower(-armSpeed);
-            } else {
-                leftSlideMotor.setPower(0);
-                rightSlideMotor.setPower(0);
-            }
-
-            // Intake speed
             if (gamepad1.a) {
-                stephanie.setPosition(intakeSpeed);
+                testServo.setPosition(.3);
+            } else  if (gamepad1.b) {
+                testServo.setPosition(.7);
             } else {
-                stephanie.setPosition(0);
-                //Continuous Servo speed
+                testServo.setPosition(0);
             }
-
-            // Claw position
-            if (gamepad1.y) {
-                goodServo.setPosition(clawOpen);
-            } else if (gamepad1.x) {
-                goodServo.setPosition(clawClose);
-            } else {
-                goodServo.setPosition(0);
-            }
-
 
 
             double botHeading = imu.getRobotYawPitchRollAngles().getYaw(AngleUnit.RADIANS);
@@ -119,12 +88,12 @@ public class TeleOp2023 extends LinearOpMode {
             double frontRightPower = (rotY - rotX - rx) / denominator;
             double backRightPower = (rotY + rotX - rx) / denominator;
 
-            lary.setPower(frontLeftPower);
+            frontLeftMotor.setPower(frontLeftPower);
             backLeftMotor.setPower(backLeftPower);
             frontRightMotor.setPower(frontRightPower);
             backRightMotor.setPower(backRightPower);
 
-            telemetry.addData("Left Bore Reading", lary.getCurrentPosition());
+            telemetry.addData("Left Bore Reading", frontLeftMotor.getCurrentPosition());
             telemetry.update();
 
         }
